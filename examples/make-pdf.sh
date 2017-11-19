9adba17edaadfcff5c77d3ef21b90c57967cd180
Alex K <alex.lumir@gmail.com>
2017-11-19 21:02:51 +0300

    Use Prawn gridlines to align numbers

---
+++ examples/make-pdf.sh
@@ -0,0 +1,1 @@
+./make-pdf.rb -t 'print("{Happy birthday, %username%!} You are awesome!")' -b '0D4F8B' -c 'E3170D' -o 'postcard.pdf'
--- make-pdf.rb
+++ make-pdf.rb
@@ -3,6 +3,7 @@
 require 'optparse'
 require 'ostruct'
 
+# require 'pry'
 require 'prawn'
 require 'prawn/measurement_extensions'
 
@@ -19,8 +20,8 @@ options = OpenStruct.new({
 
 OptionParser.new do |opt|
   opt.on('-t', '--text TEXT', 'Text to print')               { |o| options[:text] = o }
-  opt.on('-c1', '--base-color TEXT', 'Base color')           { |o| options[:base_color] = o }
-  opt.on('-c2', '--highlight-color TEXT', 'Highlight color') { |o| options[:highlight_color] = o }
+  opt.on('-b', '--base-color TEXT', 'Base color')            { |o| options[:base_color] = o }
+  opt.on('-c', '--highlight-color TEXT', 'Highlight color')  { |o| options[:highlight_color] = o }
   opt.on('-o', '--output-file TEXT', 'Output file name')     { |o| options[:output] = o }
 end.parse!
 
@@ -33,22 +34,17 @@ avg_area = PAGE_WIDTH * PAGE_HEIGHT / chars_count
 avg_side = Math.sqrt(avg_area)
 
 n_rows = (PAGE_HEIGHT / avg_side).ceil
-n_cols = (PAGE_WIDTH / avg_side).floor
+n_cols = (PAGE_WIDTH / avg_side).ceil
 
 box_w = PAGE_WIDTH.mm / n_cols
 box_h = PAGE_HEIGHT.mm / n_rows
 
-# p options.text
-# p avg_area
-# p avg_side
-# p box_w, box_h
-# p n_rows
-# p n_cols
-
 Prawn::Document.generate(options.output || 'postcard.pdf') do
   curr_color = options.base_color
-  curr_row = 1
+  curr_row = 0
   curr_col = 0
+
+  define_grid(columns: n_cols, rows: n_rows, gutter: 0.1)
 
   options.text.split('').each do |c|
     if c == OPEN_SYM
@@ -59,23 +55,31 @@ Prawn::Document.generate(options.output || 'postcard.pdf') do
       next
     end
 
-    curr_col += 1
-    if curr_col > n_cols
+    if curr_col >= n_cols
       curr_row += 1
-      curr_col = 1
+      curr_col = 0
     end
 
-    # p c.ord
+    grid_box = grid(curr_row, curr_col)
 
     float do
       bounding_box(
-        [(curr_col - 1) * box_w,
-         PAGE_HEIGHT.mm - (curr_row) * box_h],
-        width: box_w,
-        height: box_h) do
+        grid_box.top_left,
+        width: grid_box.width,
+        height: grid_box.height) do
 
-        text "#{c.ord}", color: curr_color, size: avg_side * 0.2.mm
+        text "#{c.ord}",
+             color: curr_color,
+             size: avg_side * 0.2.mm,
+             align: :center,
+             valign: :center
       end
     end
+
+    curr_col += 1
   end
+
+  # binding.pry
+  # grid.show_all
+
 end
