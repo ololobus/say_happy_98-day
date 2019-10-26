8c8d4736ba8aa38062080caac8d63c73de0ec296
Jordan Judt <dev@jordanjudt.com>
2019-10-26 18:26:25 -0500

    rufo formatting

--- make-pdf.rb
+++ make-pdf.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
-require 'ostruct'
+require "optparse"
+require "ostruct"
 
 # require 'pry'
-require 'prawn'
-require 'prawn/measurement_extensions'
+require "prawn"
+require "prawn/measurement_extensions"
 
-OPEN_SYM = '{'
-CLOSE_SYM = '}'
+OPEN_SYM = "{"
+CLOSE_SYM = "}"
 PAGE_WIDTH = 210 # mm
 PAGE_HEIGHT = 297 # mm
 
@@ -28,22 +28,22 @@ COLORS = OpenStruct.new({
 })
 
 options = OpenStruct.new({
-  base_color: 'default',
-  highlight_color: 'default_highlight',
-  text: 'Say hello {%username}!'
+  base_color: "default",
+  highlight_color: "default_highlight",
+  text: "Say hello {%username}!",
 })
 
 OptionParser.new do |opt|
-  opt.on('-t', '--text TEXT', 'Text to print')               { |o| options[:text] = o }
-  opt.on('-b', '--base-color TEXT', 'Base color')            { |o| options[:base_color] = o }
-  opt.on('-c', '--highlight-color TEXT', 'Highlight color')  { |o| options[:highlight_color] = o }
-  opt.on('-o', '--output-file TEXT', 'Output file name')     { |o| options[:output] = o }
+  opt.on("-t", "--text TEXT", "Text to print") { |o| options[:text] = o }
+  opt.on("-b", "--base-color TEXT", "Base color") { |o| options[:base_color] = o }
+  opt.on("-c", "--highlight-color TEXT", "Highlight color") { |o| options[:highlight_color] = o }
+  opt.on("-o", "--output-file TEXT", "Output file name") { |o| options[:output] = o }
 end.parse!
 
 chars_count = options
   .text
-  .gsub(OPEN_SYM, '')
-  .gsub(CLOSE_SYM, '')
+  .gsub(OPEN_SYM, "")
+  .gsub(CLOSE_SYM, "")
   .size
 avg_area = PAGE_WIDTH * PAGE_HEIGHT / chars_count
 avg_side = Math.sqrt(avg_area)
@@ -68,14 +68,14 @@ def get_hex_color(color)
   end
 end
 
-Prawn::Document.generate(options.output || 'postcard.pdf') do
+Prawn::Document.generate(options.output || "postcard.pdf") do
   curr_color = get_hex_color(options.base_color)
   curr_row = 0
   curr_col = 0
 
   define_grid(columns: n_cols, rows: n_rows, gutter: 0.1)
 
-  options.text.split('').each do |c|
+  options.text.split("").each do |c|
     if c == OPEN_SYM
       curr_color = get_hex_color(options.highlight_color)
       next
@@ -95,8 +95,8 @@ Prawn::Document.generate(options.output || 'postcard.pdf') do
       bounding_box(
         grid_box.top_left,
         width: grid_box.width,
-        height: grid_box.height) do
-          
+        height: grid_box.height,
+      ) do
         text "#{c.ord}",
              color: curr_color,
              size: avg_side * 0.2.mm,
